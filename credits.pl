2deba8deefeee3987525f9758a5a84d551e12a6f
waylandsmith <mtm10@hampshire.edu>
2012-04-08 15:56:07 -0400

    Added a new and improved wolf! Added a credits script which will someday display our credits and license. For the moment, it's just a placeholder.

---
+++ credits.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+
+$content = <<fin;
+
+This work is the product of Matthew Meneghini and Matt Pink
+Originally produced for CS-112 at Hampshire College in Spring 2012
+
+Full credits forthcoming.  This code is still in development.
+
+fin
+
+
+print $content;
