85b153ba025116d12eed10f77ef580666b467d1b
waylandsmith <mtm10@hampshire.edu>
2012-04-08 12:56:51 -0400

    Added asset directories, rough sketch of player as an xcf and png.  PNG will suffice for vertical slice 1

---
+++ assets/mini/vs1.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+$message = <<END;
+
+screen size = 800 by 600
+goals:  have a 'first fight' working for the fight minigame
+have a wolf that bites
+have an axe
+have basic stats
+have health
+
+END
+
+print $message;
