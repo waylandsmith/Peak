0ce345e40800b754c9bb2d66f8072ce5c6e81b02
waylandsmith <mtm10@hampshire.edu>
2012-04-08 16:19:08 -0400

    Added a temporary storage directory for dialog and written script. Forgive the Perl...just a little fun.

---
+++ assets/script/first_encounter.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+$dialogPlan = <<fin;
+
+Intro:
+----------------------------------
+A wounded wolf is attacking!
+----------------------------------
+----------------------------------
+The PC has ventured to their shed
+to get some wood for a fire, and
+disturbed an injured wolf hiding
+there for shelter.
+---------------------------------
++++++++++++++++++++++++++++++++++
+---------------------------------
+Combat:
+---------------------------------
+The wolf bit at me.
+---------------------------------
+I dodged the fangs....
+---------------------------------
+I was struck!
+---------------------------------
+I swung my axe,
+---------------------------------
+The wolf ducked under my blow...
+---------------------------------
+I found my target with steel.
+---------------------------------
+I was hurt!
+---------------------------------
+The wolf was slain
+
+
+fin
+
+print $dialogPlan;
